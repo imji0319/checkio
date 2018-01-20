b5fd8dc64d3ac4a55baa5e046510903e05fdb846
jihye <jihye@192.168.0.140>
2018-01-20 10:48:39 +0900

    checkio site

--- Checkio/elementary/Number_base.py
+++ Checkio/elementary/Number_base.py
@@ -1,6 +1,23 @@
 '''
-Created on 2018. 1. 15.
+Created on 2018. 1. 20.
 
 @author: jihye
 '''
-print(int("AB",10))
+def checkio(str_numbers,radix):
+    
+    # 예외 처리 try - except error종류 
+    try:
+        return (int(str_numbers,radix))
+    except ValueError:
+        return -1 
+    
+
+        
+if __name__ == '__main__':
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A = 10"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
